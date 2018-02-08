895ad9f6e96c847431835515a73e76755afda2d2
SWETHA BADAM <sweth.badam@gmail.com>
2018-02-08 18:42:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,52 @@ return c
   
 }
 subtract(1,81);
+function multiply(a,b){
+  var c=a*b;
+return c
+  
+}
+multiply(1,81);
+function divide(a,b){
+  var c=a/b;
+return c
+  
+}
+divide(1,81);
+
+function inc(n){
+  n++;
+  return n;
+}
+inc(5);
+
+function dec(n){
+  n--;
+  return n;
+}
+dec(5);
+
+function makeInt(n){
+  
+  
+  return parseInt(n,10);
+}
+makeInt(4);
+makeInt(saded);
+
+
+
+
+
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
+preserveDecimal(2.22);
+
+
+
+
+
+
+
